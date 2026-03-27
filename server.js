914bd410546b412deb9ab02270911ac3f1af1ce7
hathuan83-ui <hathuan83@gmail.com>
2026-03-27 22:33:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-import express from 'express';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
